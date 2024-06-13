3bea1caa39812202e328dc9b577a7f9c5f00a56c
unknown <ezejistellan@gmail.com>
2024-06-13 15:10:41 +0100

    week 4

---
+++ week_4.py
@@ -0,0 +1,41 @@
+# #loops
+# #function
+# print('hello')
+# #classes
+# for i in range(6):
+    # print (i)
+    # for i in range(6):
+    #     print(i)
+    #     #print(name.upper())
+    #     #print(type(name))
+    #     for num in range(1,11):
+    #     print(num)
+
+
+
+# for num in range(2):
+#     print(num )
+
+# for num in range(3,39,3):
+#     print(num)
+
+#     for num in range(6):
+#         # print(num)
+#         # print('Dami')
+
+# for num in range(1,12):
+#     print(num*3)  
+
+# name= "Damilolannsndndmdm"
+# for i in range(len(name)):
+#      print(name[11])
+LG_in_Kwara = ['Ilorin west', 'Ekiti', 'oke-oro', 'ifelodun', 'barutee', 'asa']
+
+for LG in LG_in_Kwara:
+     print(LG[-1])
+
+for i in range(len(LG_in_Kwara)):
+     print(LG_in_Kwara[i][-1])
+
+
+        
