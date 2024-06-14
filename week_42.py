8195f9e99d2e9b9177a975ee631b629e3c9e4f92
unknown <ezejistellan@gmail.com>
2024-06-14 13:06:06 +0100

    update

---
+++ week_42.py
@@ -0,0 +1,55 @@
+# LG_in_Kwara = ['Ilorin west','Ekiti', 'oke-oro', 'ifelodun', 'barutee', 'asa','ilorin south','kaima']
+# length =len(LG_in_Kwara)
+# for i in range(length):
+#     print(LG_in_Kwara[1:8:2])
+    
+
+
+#     # for lg in LG_in_Kwara:
+#     #   print(lg)
+# for i in range(length):
+#     if i % 2 ==1:
+#         print(LG_in_Kwara[i])
+
+#         if(i+1) % 2==0:
+#             print(LG_in_Kwara[i])
+
+
+#             for i in range(length):
+#                 if i % 2==0:
+#                   print(LG_in_Kwara[i])
+#                    #code 2
+# # for i in range(0,length,2):
+# #       print(LG_in_Kwara[i])
+def double(x):
+   print(x*2)
+   
+# # # lga_ starting_with_vowels
+# # vowels= 'aeiou'
+# # for char in vowels:
+# #     print(char)
+ # for i in range(length):
+#     if LG_in_Kwara
+# LG_in_Kwara = ['Ilorin west','Ekiti', 'oke-oro', 'ifelodun', 'barutee', 'asa','ilorin south','kaima']
+# length =len(LG_in_Kwara)
+# vowels='aeiou'
+# for i in range(length):
+#     if LG_in_Kwara[i][0] in vowels:
+#       print(LG_in_Kwara[i])
+
+#       #code 2
+# for lg in LG_in_Kwara:
+#   if lg[0]in vowels:
+#      print(lg) 
+
+double(100)
+
+double(4.2)
+   
+
+
+
+
+
+    
+
